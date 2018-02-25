a45232b362369d55066d7712adec6e984a5acad8
padedou <padedou@gmail.com>
2018-02-25 17:23:04 +0200

    Creates and adds a clickable button

---
+++ src/Button.js
@@ -0,0 +1,42 @@
+export default class Button extends PIXI.Sprite {
+    constructor(x, y, width, height, counter) {
+        super();
+        this.text;
+        this.create(x, y, width, height);
+    }
+
+    create(x, y, width, height) {
+        console.log('Button create');
+        // generate the texture
+        const gfx = new PIXI.Graphics();
+        gfx.beginFill(0xffffff, 0.5);
+        gfx.drawRoundedRect(0, 0, width, height, height / 5);
+        gfx.endFill();
+        this.texture = gfx.generateCanvasTexture();
+
+        // set the x, y and anchor
+        this.x = x;
+        this.y = y;
+        this.anchor.x = 0.5;
+        this.anchor.y = 0.5;
+
+        // create the text object
+        this.text = new PIXI.Text();
+        this.text.anchor = new PIXI.Point(0.5, 0.5);
+        this.addChild(this.text);
+
+        this.interactive = true;
+        this.on('click', this.clicked);
+    }
+
+    setText(val, style) {
+        // Set text to be the value passed as a parameter
+        this.text.text = val;
+        // Set style of text to the style passed as a parameter
+        this.text.style = style;
+    }
+
+    clicked() {
+        console.log('Test button clicked');
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import * as PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js';
+import Button from './Button';
 
 const app = new PIXI.Application({
     width: 350,
     height: 650,
     backgroundColor: 0x061639
 });
+
+const button = new Button(50, 50, 100, 100);
+
+button.setText("Test Button");
+app.stage.addChild(button);
 
 document.body.appendChild(app.view);
