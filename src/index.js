a963d92425c3cf6e81489201bf1af0550b70f4cb
padedou <padedou@gmail.com>
2018-02-26 21:59:12 +0200

    - Changes background color. - Adds a boolean to keep track of which is playing (X/O). - Adds getXIsPlaying() and moveMade() functions to be used as callbacks by the tiles.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import * as PIXI from 'pixi.js';
 import Button from './Button';
+import Tile from './Tile';
 
 const app = new PIXI.Application({
     width: 350,
     height: 650,
-    backgroundColor: 0x061639
+    backgroundColor: 0x333333
 });
+let xIsPlaying = true;
 
 document.body.appendChild(app.view);
+
+function getXIsPlaying() {
+    return true && xIsPlaying;
+}
+
+function moveMade() {
+    console.log('moveMade called');
+    xIsPlaying = !xIsPlaying;
+}
