7a6dacec02d42f791d9ccf934d95e26d3490497c
padedou <padedou@gmail.com>
2018-02-26 22:02:23 +0200

    Creates the Tile class

--- src/Tile.js
+++ src/Tile.js
@@ -1,3 +1,9 @@
 import Button from './Button';
 
-export default class Tile extends Button {};
+export default class Tile extends Button {
+    constructor(x, y, width, height, cbXIsPlaying, cbMoveMade) {
+        super(x, y, width, height, 0x2B4A6F, 1, '');
+        this.cbXIsPlaying = cbXIsPlaying;
+        this.cbMoveMade = cbMoveMade;
+    }
+};
