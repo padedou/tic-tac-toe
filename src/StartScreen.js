8eae26bd78a7531f4b0245dbbca5fb293d32e30c
padedou <padedou@gmail.com>
2018-02-27 21:22:03 +0200

    Sets the mouse hover event handlers

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -6,18 +6,26 @@ const txtStyleButtons = {fill: colorText};
 export default class StartScreen extends PIXI.Container {
     constructor(appWidth, appHeight) {
         super();
+        
         this.btnSelectX = new Button(0, appHeight / 2, appWidth / 2, 50, colorTile, 1, 'X', txtStyleButtons);
-        this.btnSelectO = new Button(appWidth / 2, appHeight / 2, appWidth / 2, 50, colorTile, 1, 'O');
-        this.btnSelectO.txtStyle = txtStyleButtons;
-        this.btnSelectX.on('mouseover', () => {
-            this.btnSelectX.color = colorMarked;
-            this.btnSelectX.generateTextureAndText();
-        });
-        this.btnSelectX.on('mouseout', () => {
-            this.btnSelectX.color = colorTile;
-            this.btnSelectX.generateTextureAndText();
-        });
+        this.btnSelectO = new Button(appWidth / 2, appHeight / 2, appWidth / 2, 50, colorTile, 1, 'O', txtStyleButtons);
+        
+        this.btnSelectX.on('mouseover', this.handleMouseOver);
+        this.btnSelectO.on('mouseover', this.handleMouseOver);
+        this.btnSelectX.on('mouseout', this.handleMouseOut);
+        this.btnSelectO.on('mouseout', this.handleMouseOut);
+
         this.addChild(this.btnSelectX);
         this.addChild(this.btnSelectO);
+    }
+
+    handleMouseOver(e) {
+        e.currentTarget.color = colorMarked;
+        e.currentTarget.generateTextureAndText();
+    }
+
+    handleMouseOut(e) {
+        e.currentTarget.color = colorTile;
+        e.currentTarget.generateTextureAndText();
     }
 }
