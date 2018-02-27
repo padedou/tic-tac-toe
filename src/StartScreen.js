869e1f93c5711018e1c6421658366f840a860428
padedou <padedou@gmail.com>
2018-02-27 20:57:34 +0200

    - Adds two buttons. - Changes the color of the one of the buttons when mouse hovers.

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,3 +1,23 @@
+import Button from './Button';
+import { colorText, colorTile, colorMarked } from './colors';
+
+const txtStyleButtons = {fill: colorText};
+
 export default class StartScreen extends PIXI.Container {
-    
+    constructor(appWidth, appHeight) {
+        super();
+        this.btnSelectX = new Button(0, appHeight / 2, appWidth / 2, 50, colorTile, 1, 'X', txtStyleButtons);
+        this.btnSelectO = new Button(appWidth / 2, appHeight / 2, appWidth / 2, 50, colorTile, 1, 'O');
+        this.btnSelectO.txtStyle = txtStyleButtons;
+        this.btnSelectX.on('mouseover', () => {
+            this.btnSelectX.color = colorMarked;
+            this.btnSelectX.generateTextureAndText();
+        });
+        this.btnSelectX.on('mouseout', () => {
+            this.btnSelectX.color = colorTile;
+            this.btnSelectX.generateTextureAndText();
+        });
+        this.addChild(this.btnSelectX);
+        this.addChild(this.btnSelectO);
+    }
 }
