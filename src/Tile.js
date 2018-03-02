777f6c46a2df4deed9fd26b27703b75ae94344a1
padedou <padedou@gmail.com>
2018-03-02 16:52:40 +0200

    Sets the text style fot the tiles

--- src/Tile.js
+++ src/Tile.js
@@ -1,15 +1,21 @@
 import Button from './Button';
+import { colorText } from './colors'
 
 export default class Tile extends Button {
     constructor(x, y, width, height, cbXIsPlaying, cbMoveMade) {
         super(x, y, width, height, 0x2B4A6F, 1, '');
         this.cbXIsPlaying = cbXIsPlaying;
         this.cbMoveMade = cbMoveMade;
+        this.txtStyleOptions = {
+            fill: colorText,
+            fontSize: (this.width) / 2,
+            fontWeight: 'bold'
+        };
         this.on('click', () => {
             if (this.txtDescription === '') {
                 cbXIsPlaying() ? this.txtDescription = 'X' : this.txtDescription = 'O';
                 this.generateTextureAndText();
-                this.cbMoveMade();    
+                this.cbMoveMade(this.id);
             }
         });
     }
